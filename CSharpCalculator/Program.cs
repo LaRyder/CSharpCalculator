5cf390c5a3570aadb5ebb445d35cad21b413fd12
Lauren Ryder <lauren.ryder@microsoft.com>
2021-08-24 08:47:52 -0400

    adding foreground color

--- CSharpCalculator/Program.cs
+++ CSharpCalculator/Program.cs
@@ -7,49 +7,56 @@ namespace CSharpCalculator
     {
         static void Main(string[] args)
         {
+
             try
             {
-                int number1; 
-                int number2 = 0; 
+                int number1;
+                int number2 = 0;
 
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Welcome to Lauren's Calculator");
 
-                Console.WriteLine("Enter first integer > Press Enter", Color.Blue);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine("Enter first integer > Press Enter");
                 number1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter Second integer > Press Enter", Color.Blue);
+                Console.WriteLine("Enter Second integer > Press Enter");
                 number2 = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("How would you like me to calculate?");
-                Console.WriteLine(" - Add + ", Color.Blue);
-                Console.WriteLine(" - Subtract - ", Color.Blue);
-                Console.WriteLine(" - Multiply * ", Color.Blue);
-                Console.WriteLine(" - Divide / ", Color.Blue);
+                Console.WriteLine(" - Add + ");
+                Console.WriteLine(" - Subtract - ");
+                Console.WriteLine(" - Multiply * ");
+                Console.WriteLine(" - Divide / ");
 
                 switch (Console.ReadLine())
                 {
                     case "+":
-                        Console.WriteLine($" Your answer = " + (number1 + number2) , Color.Green);
+                        Console.WriteLine($" Your answer = " + (number1 + number2));
                         break;
                     case "-":
-                        Console.WriteLine($" Your answer = " + (number1 - number2), Color.Green);
+                        Console.WriteLine($" Your answer = " + (number1 - number2));
                         break;
                     case "*":
-                        Console.WriteLine($" Your answer = " + (number1 * number2), Color.Green);
+                        Console.WriteLine($" Your answer = " + (number1 * number2));
                         break;
                     case "/":
-                        Console.WriteLine($" Your answer = " + (number1 / number2), Color.Green);
+                        Console.WriteLine($" Your answer = " + (number1 / number2));
                         break;
                 }
 
-                        Console.ReadKey();
+                Console.ReadKey();
             }
 
             catch (FormatException e)
             {
-                Console.WriteLine("Oops, this calculator is numbers only!", Color.Red);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("  :(  Oops, this calculator uses numbers only!  :(  ");
+                
             }
-               
         }
     }
 }
