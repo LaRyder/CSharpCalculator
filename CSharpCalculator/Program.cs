031186ea9d6369acc0b233dd14fab41e61dc4209
Lauren Ryder <lauren.ryder@microsoft.com>
2021-08-24 09:02:55 -0400

    adding foreground color

--- CSharpCalculator/Program.cs
+++ CSharpCalculator/Program.cs
@@ -13,12 +13,14 @@ namespace CSharpCalculator
                 int number1;
                 int number2 = 0;
 
+                /* Setting Colors and creating title*/
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Welcome to Lauren's Calculator");
 
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.Blue;
 
+                /* Creating calculator input*/
                 Console.WriteLine("Enter first integer > Press Enter");
                 number1 = Convert.ToInt32(Console.ReadLine());
 
@@ -31,6 +33,7 @@ namespace CSharpCalculator
                 Console.WriteLine(" - Multiply * ");
                 Console.WriteLine(" - Divide / ");
 
+                /* Creating a switch case for calculator output*/
                 switch (Console.ReadLine())
                 {
                     case "+":
@@ -49,13 +52,13 @@ namespace CSharpCalculator
 
                 Console.ReadKey();
             }
-
+            /* Creating exception for non-numnerical characters*/
             catch (FormatException e)
+
             {
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("  :(  Oops, this calculator uses numbers only!  :(  ");
-                
             }
         }
     }
